6cb596cae375171c4b453f9f8589f187b14e7864
pedroabreu2359 <pedromchabreu@hotmail.com>
2024-05-16 17:39:15 -0300

    Adicionadas seções de Início, Cardápio e Fale Conosco

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-let prevScrollPos = window.pageYOffset;
-
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollPos = currentScrollPos;
-}
-
 function reserveTable() {
-    const name = document.getElementById('name').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-    const guests = document.getElementById('guests').value;
+  const name = document.getElementById('name').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+  const guests = document.getElementById('guests').value;
 
-    if (name && date && time && guests) {
-        const message = `Olá, ${name}! Sua reserva para ${guests} pessoa(s) na data ${date} às ${time} foi confirmada.`;
-        document.getElementById('message').innerText = message;
-    } else {
-        document.getElementById('message').innerText = 'Por favor, preencha todos os campos.';
-    }
+  if (name && date && time && guests) {
+      document.getElementById('message').innerText = `Reserva confirmada para ${name} em ${date} às ${time} para ${guests} pessoa(s).`;
+  } else {
+      document.getElementById('message').innerText = 'Por favor, preencha todos os campos.';
+  }
 }
